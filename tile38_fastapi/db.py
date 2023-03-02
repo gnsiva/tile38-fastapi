157a6aa8ec20a1f7ed63910d57394ccb13d698d9
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2023-03-02 23:29:38 +0000

    filler updated to work with and fill PostGIS

---
+++ tile38_fastapi/db.py
@@ -0,0 +1,31 @@
+"""File containing additional code for working with MySQL and PostGIS"""
+from geoalchemy2 import Geometry as PostGisGeometry
+from psycopg2.errors import DuplicateTable
+from sqlalchemy import Engine, create_engine, Column, Integer
+from sqlalchemy.exc import ProgrammingError
+from sqlalchemy.ext.declarative import declarative_base
+
+
+def create_postgis_engine() -> Engine:
+    return create_engine("postgresql+psycopg2://the_user:the_password@localhost:5432/parking")
+
+
+Base = declarative_base()
+
+
+class ParkingLocations(Base):
+    __tablename__ = "parking_locations"
+
+    parking_location_id = Column(Integer, primary_key=True, nullable=False)
+    price = Column(Integer, nullable=False)
+    geometry = Column(PostGisGeometry, nullable=False)
+
+    @classmethod
+    def create_table(cls) -> None:
+        """Create table if exists"""
+        engine = create_postgis_engine()
+        try:
+            cls.__table__.create(bind=engine)
+        except (DuplicateTable, ProgrammingError):
+            pass
+        engine.dispose()
--- tile38_fastapi/filler.py
+++ tile38_fastapi/filler.py
@@ -14,41 +14,98 @@ STATS parking-locations
 """
 import csv
 import json
+import time
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Callable
 
 import redis
+from geoalchemy2.elements import WKTElement
 from shapely import wkt
 from shapely.geometry import mapping
+from sqlalchemy.orm import sessionmaker
 
 from tile38_fastapi import PARKING_LOCATIONS
+from tile38_fastapi.db import create_postgis_engine, ParkingLocations
+
+
+@dataclass
+class Row:
+    parking_location_id: int
+    geometry: str
+    price: int
+
+    def to_postgis(self) -> ParkingLocations:
+        return ParkingLocations(
+            parking_location_id=self.parking_location_id,
+            price=self.price,
+            geometry=WKTElement(self.geometry),
+        )
+
+
+def timer(name: str):
+    def timer_(func: Callable):
+        def wrapper(*args, **kwargs):
+            start = time.perf_counter()
+            func(*args, **kwargs)
+            diff = time.perf_counter() - start
+            print(f"Completed writing to {name} in {diff:.1f} seconds")
+        return wrapper
+    return timer_
+
+
+@timer(name="tile38")
+def write_rows_to_tile38(rows: list[Row]) -> None:
+    c = redis.Redis(host="localhost", port=9851, single_connection_client=True)
+    for row in rows:
+        d = mapping(wkt.loads(row.geometry))
+        d["price"] = row.price
+
+        command = [
+            "SET",                    # The Tile38 operation to perform
+            PARKING_LOCATIONS,        # Tile38 key
+            row.parking_location_id,  # Tile38 field
+            "OBJECT",                 # The type of data to store (GeoJSON object in this case)
+            json.dumps(d),            # Tile38 value
+        ]
+
+        c.execute_command(*command)
+
+
+@timer(name="postgis")
+def write_rows_to_postgis(rows: list[Row]) -> None:
+    engine = create_postgis_engine()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # it may be faster to use sqlalchemy core here instead of orm
+    # as the focus is the performance of data retrieval I'm leaving this as is
+    session.add_all([
+        row.to_postgis()
+        for row in rows
+    ])
+
+    session.commit()
+    session.close()
 
 
 def main(input_file: Path) -> None:
 
-    c = redis.Redis(host="localhost", port=9851)
+    # create postgis table
+    ParkingLocations.create_table()
 
+    # load the test data from a file
     with open(input_file, "r") as f:
 
         # skip the header row of CSV
         next(csv.reader(f))
 
-        for row in csv.reader(f):
+        # read the rest of the rows into a list of data classes
+        rows = [Row(*row) for row in csv.reader(f)]
 
-            # unpack data and create geojson object to insert into Tile38
-            parking_location_id, geometry, price = row
-            d = mapping(wkt.loads(geometry))
-            d["price"] = price
-            geojson_object = json.dumps(d)
+        write_rows_to_tile38(rows)
 
-            command = [
-                "SET",
-                PARKING_LOCATIONS,    # Tile38 key
-                parking_location_id,  # Tile38 field
-                "OBJECT",
-                geojson_object,       # Tile38 value
-            ]
-
-            c.execute_command(*command)
+        write_rows_to_postgis(rows)
 
 
 if __name__ == "__main__":
