4b3771452f4b9a4578d25ec355e8c70888104dd9
Ganesh N. Sivalingam <g.n.sivalingam@gmail.com>
2023-03-06 16:24:05 +0000

    updated the simplest version to match the presentation

--- tile38_fastapi/api_v1.py
+++ tile38_fastapi/api_v1.py
@@ -8,10 +8,8 @@ Example query below
 http://127.0.0.1:8000/nearby-parking-locations?latitude=11.7044525&longitude=48.0449558&distance_meters=1500
 """
 import uvicorn
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import redis
-
-from tile38_fastapi import PARKING_LOCATIONS
 
 
 app = FastAPI()
@@ -19,17 +17,14 @@ app = FastAPI()
 
 @app.get("/nearby-parking-locations")
 async def get_nearby(
-        latitude: float = Query(ge=-90, le=90),
-        longitude: float = Query(ge=-180, le=180),
-        distance_meters: float = Query(
-            ge=0.1,
-            description="Distance from point to search for parking locations",
-        ),
+        latitude: float,
+        longitude: float,
+        distance_meters: int,
 ):
     c = redis.Redis(host="localhost", port=9851, single_connection_client=True)
 
     return c.execute_command(
-        "NEARBY", PARKING_LOCATIONS, "POINT", longitude, latitude, distance_meters)
+        "NEARBY", "parking-locations", "POINT", longitude, latitude, distance_meters)
 
 
 if __name__ == "__main__":
